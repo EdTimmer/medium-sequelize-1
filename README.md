ce1d04afa7da98db9d91b82551b6056d2db20b63
Edward Timmer <edtimmer@gmail.com>
2018-06-17 12:57:41 -0400

    have all code

---
+++ README.md
@@ -0,0 +1,2 @@
+1) Create a postgres database named 'ships_db' (e.g., run 'createdb ships_db');
+2) Run 'npm install' to download node_modules, Sequelize, and postgres.
--- db.js
+++ db.js
@@ -1,36 +1,132 @@
 const Sequelize = require('sequelize');
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/members_ships_db');
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/ships_db');
 
 const Ship = conn.define('ship', {
-  name: Sequelize.STRING,
-  purpose: Sequelize.STRING
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  purpose: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
 });
 
 const Member = conn.define('member', {
-  name: Sequelize.STRING,
-  species: Sequelize.STRING
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  species: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
 });
 
-const createShips = () => { 
+const seed = () => {
   return Promise.all([
-    Ship.create({ name: 'Enterprise', purpose: 'Explore Strange New Worlds' }),
-    Ship.create({ name: 'Planet Express', purpose: 'Package Delivery' })
-  ]);
-}
-
-const createMembers = () => {
-  return Promise.all([
-    Member.create({ name: 'Kirk', species: 'Human' }),
-    Member.create({ name: 'Spock', species: 'Human-Vulcan Hybrid' }),
-    Member.create({ name: 'McCoy', species: 'Human' }),
-    Member.create({ name: 'Leela', species: 'Mutant' }),
-    Member.create({ name: 'Fry', species: 'Human' }),
-    Member.create({ name: 'Bender', species: 'Robot' })
-  ]);
-}
+    Ship.create({ name: 'Enterprise', purpose: 'exploration' }),
+    Ship.create({ name: 'Planet Express', purpose: 'delivery' }),
+    Member.create({ name: 'Kirk', species: 'human' }),
+    Member.create({ name: 'Spock', species: 'hybrid' }),
+    Member.create({ name: 'McCoy', species: 'human' }),
+    Member.create({ name: 'Leela', species: 'mutant' }),
+    Member.create({ name: 'Fry', species: 'human' }),
+    Member.create({ name: 'Bender', species: 'robot' })
+  ])
+  .then(([enterprise, planetexpress, kirk, spock, mccoy, leela, fry, bender]) => {
+    return Promise.all([
+      kirk.setShip(enterprise),
+      spock.setShip(enterprise),
+      mccoy.setShip(enterprise),
+      leela.setShip(planetexpress),
+      fry.setShip(planetexpress),
+      bender.setShip(planetexpress)
+    ]);
+  })
+  .catch((error) => console.log(error))
+};
 
 Member.belongsTo(Ship);
+Ship.hasMany(Member);
+
+//SEARCH FOR PUROPSE OF PLANET EXPRESS:
 
 conn.sync({ force: true })
-  .then(() => createShips())
-  .then(() => createMembers());
+  .then(() => seed())
+  .then(() => Ship.findOne({
+    where: {
+      name: 'Planet Express'
+    }
+  }))
+  .then(ship => console.log(ship.purpose))
+  .catch(error => console.log(error));
+
+//SEARCH FOR ROBOT:
+
+// conn.sync({ force: true })
+//   .then(() => seed())
+//   .then(() => Member.findOne({
+//     where: {
+//       species: 'robot'
+//     }
+//   }))
+//   .then(member => console.log(member.name))
+//   .catch(error => console.log(error));
+
+
+//SEARCH FOR LEELA'S SHIP
+
+// conn.sync({ force: true })
+//   .then(() => seed())
+//   .then(() => Member.findOne({
+//     where: {
+//       name: 'Leela'
+//     }
+//   }))
+//   .then(member => Ship.findById(member.shipId))
+//   .then(ship => console.log(ship.name))
+//   .catch((error) => console.log(error));
+
+//SEARCH FOR LEELA'S SHIP, AN EASY WAY
+
+// conn.sync({ force: true })
+//   .then(() => seed())
+//   .then(() => Member.findOne({
+//     where: {
+//       name: 'Leela'
+//     }
+//   }))
+//   .then(member => member.getShip())
+//   .then(ship => console.log(ship.name))
+//   .catch((error) => console.log(error));
+
+//SEARCH FOR ENTERPRISE CREW
+
+// conn.sync({ force: true })
+//   .then(() => seed())
+//   .then(() => Ship.findOne({
+//     where: {
+//       name: 'Enterprise'
+//     }
+//   }))
+//   .then(ship => Member.findAll({
+//     where: {
+//       shipId: ship.id
+//     }
+//   }))
+//   .then(members => members.forEach( member => console.log(member.name)))
+//   .catch((error) => console.log(error));
+
+//SEARCH FOR ENTERPRISE CREW, AN EASY WAY
+
+// conn.sync({ force: true })
+//   .then(() => seed())
+//   .then(() => Ship.findOne({
+//     where: {
+//       name: 'Enterprise'
+//     }
+//   }))
+//   .then(ship => ship.getMembers())
+//   .then(members => members.forEach( member => console.log(member.name)))
+//   .catch((error) => console.log(error));
